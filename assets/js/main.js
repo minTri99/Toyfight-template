a0628ed984aadeb49034cace49e01b88be956f35
Le Minh Tri <minhtri0795@gmail.com>
2021-06-04 23:03:19 +0700

    change project to PandaMonk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
-    
-
+  // Window scroll event
+  window.addEventListener("scroll",function(){
+    if(scrollY>=80){
+      gsap.to(".navBTT",{
+        duration:0.1,
+        ease: "power2",
+        marginTop:"0px",
+        maxWidth:"500px",
+        stagger: 0.05,
+      })
+    }else{
+      gsap.to(".navBTT",{
+        duration:0.1,
+        ease: "power2",
+        marginTop:"",
+        maxWidth:"",
+        stagger: 0.05,
+      })
+    }
+  })
+  // 
 });
